929c554643831f5324f7cafe4e8ad8c7023486e2
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2022-01-29 12:10:59 +0200

    Update and rename slides.js to script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+let img_src = ["img/1.jpg", "img/2.jpg", "img/3.jpg","img/4.jpg"];
+
+i = 0;
+let slider = document.getElementById("slider");
+
+let timer;
+
+let effect = "size";
+
+function next() {
+  i++;
+  if (i >= img_src.length) i = 0;
+  addEffect();
+  setTimeout(change, 1000);
+}
+
+function start() {
+  stop();
+  timer = setInterval(next, 2000);
+}
+
+function stop() {
+  clearInterval(timer);
+}
+
+function addEffect() {
+  slider.classList.add(effect);
+}
+
+function change() {
+  slider.src = img_src[i];
+  slider.classList.remove(effect);
+}
+
+let ef = document.forms["effects"];
+for (let i = 0; i < ef.length; i++) {
+     ef[i].addEventListener("change", changeEffect);
+}
+
+function changeEffect(event) {
+   effect = event.target.value;
+}
